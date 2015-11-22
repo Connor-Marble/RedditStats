92288eb06a3be153095ebe5630e1e386fb8a3d9b
Connor-Marble <MarbleConnor@gmail.com>
2015-11-21 22:55:08 -0500

    added routing to angular app

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
-var app = angular.module('redditStats', [])
+var app = angular.module('redditStats', ['ngRoute'])
+
 
 app.controller('StartController', function ($scope) {
     $scope.userName = "Example";
+});
+
+app.controller('AboutController', function ($scope) {
+
+});
+
+
+app.config(function($routeProvider) {
+    $routeProvider
+
+	.when('/', {
+	    templateUrl : './templates/start.html',
+	    controller : 'StartController'
+	})
+    
+	.when('/about', {
+	    templateUrl : './templates/about.html',
+	    controller : 'AboutController'
+	});
 });
